8626c901bba23ad7e5330cf91eb7e3aaa49503ac
Роман Мстиславский <mstislavskijr@gmail.com>
2025-10-03 22:21:58 +0300

    fix-23

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path  # type: ignore
+from django.urls import path
 from catalog import views
+from . import views
 
-app_name = 'catalog'
+# app_name = 'catalog'
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('home/', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
     path('contact_post/', views.contact_post, name='contact_post'),
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def index(request):
+    return render(request, 'catalog/index.html')
+
 def home(request):
     return render(request, 'catalog/home.html')
 
