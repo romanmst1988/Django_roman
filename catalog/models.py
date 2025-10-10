c05910e2019fc117e8c9435e296a8a12e9c0c406
Роман Мстиславский <mstislavskijr@gmail.com>
2025-10-10 15:19:54 +0300

    created parametr models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Catalog(models.Model):
+    name = models.CharField(max_length=100)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='catalogs')
+
+    def __str__(self):
+        return self.name
+
+# Поля моделей и Параметры полей
+
+    # first_name = models.CharField(max_length=150, verbose_name='Недвижимость')
+    # Last_name = models.CharField(max_length=150, verbose_name='Вид', unique=True)
+    #
+    # age = models.IntegerField(help_text='Введите возраст постройки')
+    # is_active = models.BooleanField(default=True)
+    # description = models.TextField(null=True, blank=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # image = models.ImageField(upload_to='photos/', verbose_name='Фотография')
+    # group = models.ForeignKey('Group', on_delete=models.CASCADE, related_name='catalogs')
+    # profile = models.OneToOneField('Profile', on_delete=models.CASCADE)
+    # tags = models.ManyToManyField('Tag')
+    #
+    # STATUS_CHOICES = [
+    #     ('draft', 'Draft'),
+    #     ('published', 'Published'),
+    # ]
+    # status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    #
+    #
+    # def __str__(self):
+    #     return f"{self.first_name} {self.Last_name}"
+    #
+    # class Meta:
+    #     verbose_name = 'Каталог'
+    #     verbose_name_plural = 'Каталоги'
+    #     ordering = ['first_name']
+    #     db_table = 'custom_table_name'
+
+
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import RedirectView
 from catalog import views as catalog_views
 
@@ -8,3 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
