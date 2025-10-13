16d910fb1657631138bfaec2c98f9c7d9a8653c6
Роман Мстиславский <mstislavskijr@gmail.com>
2025-10-13 18:01:02 +0300

    trening_3

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import Catalog
 
 # самый простой способ
-# admin.site.register(Catalog)
+# admin.site.register(Catalog) # регистрируем модель в админке
 
 
 # второй простой способ обширный и гибкий с декоратором
-@admin.register(Catalog)
+@admin.register(Catalog) # декоратор для регистрации модели
 class CatalogAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'year')
-    list_filter = ('year',)
-    search_fields = ('first_name', 'last_name',)
+    list_filter = ('year',) # фильтр по году (добавить в строку поиска)
+    search_fields = ('first_name', 'last_name',) # поиск по имени и фамилии
