05d65048b23150dbad54f18b719eb4610ed5341c
Роман Мстиславский <mstislavskijr@gmail.com>
2025-10-14 01:27:05 +0300

    modified catalog/admin.py; modified catalog/models.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-from .models import Catalog
+from .models import Product, Category
 
 # самый простой способ
-# admin.site.register(Catalog) # регистрируем модель в админке
-
+# admin.site.register(Product, Category) # регистрируем модель в админке
 
 # второй простой способ обширный и гибкий с декоратором
-@admin.register(Catalog) # декоратор для регистрации модели
-class CatalogAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'year')
-    list_filter = ('year',) # фильтр по году (добавить в строку поиска)
-    search_fields = ('first_name', 'last_name',) # поиск по имени и фамилии
+@admin.register(Product) # декоратор для регистрации модели
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'image', 'category', 'price', 'created_at', 'updated_at') # поля для отображения в админке
+    list_display_links = ('name', 'description') # ссылки на редактирование
+    list_editable = ('price',) # поля для редактирования
+    list_per_page = 10 # количество записей на странице
+    search_fields = ('name', 'description', 'category__name') # поиск по имени, описанию и категории
+    list_filter = ('category', 'created_at', 'updated_at') # фильтр по категории, дате создания и изменения
+
+@admin.register(Category) # декоратор для регистрации модели
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description') # поля для отображения в админке
+    search_fields = ('name', 'description') # поиск по имени и описанию
--- catalog/models.py
+++ catalog/models.py
@@ -1,98 +1,44 @@
 from django.db import models
 
-class Catalog(models.Model):
-    FIRST_YEAR = 'Первичная недвижимость'
-    SECOND_YEAR = 'Новостройки'
-    THIRD_YEAR = 'Дома со сроком эксплуатации до X лет'
-    FOURTH_YEAR = 'Старый жилой фонд'
-
-    YEAR_IN_SCHOOL_CHOICES = [
-        (FIRST_YEAR, 'Первичная недвижимость'),
-        (SECOND_YEAR, 'Новостройки'),
-        (THIRD_YEAR, 'Дома со сроком эксплуатации до X лет'),
-        (FOURTH_YEAR, 'Старый жилой фонд'),
-    ]
-
-    first_name = models.CharField(max_length=150, verbose_name='Вид недвижимости')
-    last_name = models.CharField(max_length=150, verbose_name='Площадь (кв.м)', unique=True)
-    year = models.CharField(
-        max_length=66,
-        choices=YEAR_IN_SCHOOL_CHOICES,
-        default=FIRST_YEAR,
-        verbose_name='Состояние объекта'
-    )
+class Product(models.Model):
+    name = models.CharField(max_length=200) # Поле для названия
+    description = models.TextField(null=True, blank=True) # Поле для описания
+    image = models.ImageField(upload_to='photos/', verbose_name='Фотография') # Поле для загрузки изображения
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='products') # Связь с моделью Category
+    price = models.DecimalField(max_digits=10, decimal_places=2) #Поле для цены
+    created_at = models.DateTimeField(auto_now_add=True)  # Дата создания
+    updated_at = models.DateTimeField(auto_now=True)  # Дата последнего изменения
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return self.name
 
     class Meta:
-        verbose_name = 'Объект недвижимости'
-        verbose_name_plural = 'Объекты недвижимости'
-        ordering = ['last_name']
+        verbose_name = 'Объект Недвижимости'
+        verbose_name_plural = 'Объекты Недвижимости'
+        ordering = ['name']
 
-# Поля моделей и Параметры полей
+class Category(models.Model):
+    # first_name = models.CharField(max_length=150, verbose_name='Вид недвижимости')
+    # last_name = models.CharField(max_length=150, verbose_name='Площадь (кв.м)', unique=True)
+    name = models.CharField(max_length=150, verbose_name='Категория Недвижимости')
+    description = models.TextField(null=True, blank=True) #Поле для описания
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория Недвижимости'
+        verbose_name_plural = 'Категории Недвижимости'
+
+
+
+
+
+
+
 # class Group(models.Model):
 #     name = models.CharField(max_length=200)
 #     group = models.ForeignKey('Group', on_delete=models.CASCADE, related_name='catalogs')
 #
 #     def __str__(self):
 #         return self.name
-
-
-
-
-# class Profile(models.Model):
-#     # добавьте необходимые поля
-#     user_info = models.TextField(blank=True)
-#
-#     def __str__(self):
-#         return f"Profile {self.id}"
-#
-#
-# class Tag(models.Model):
-#     name = models.CharField(max_length=50)
-#
-#     def __str__(self):
-#         return self.name
-#
-#
-# class Catalog(models.Model):
-#         FIRST_YEAR = 'first'
-#         SECOND_YEAR = 'second'
-#         THIRD_YEAR = 'third'
-#         FOURTH_YEAR = 'fourth'
-#
-#         YEAR_IN_SCHOOL_CHOICES = [
-#             (FIRST_YEAR, 'Первичная недвижимость'),
-#             (SECOND_YEAR, 'Новостройки'),
-#             (THIRD_YEAR, 'Дома со сроком эксплуатации до X лет'),
-#             (FOURTH_YEAR, 'Старый жилой фонд'),
-#         ]
-#
-#         first_name = models.CharField(max_length=150, verbose_name='Недвижимость')
-#         Last_name = models.CharField(max_length=150, verbose_name='Вид', unique=True)
-#
-#         age = models.IntegerField(help_text='Введите возраст постройки')
-#         is_active = models.BooleanField(default=True)
-#         description = models.TextField(null=True, blank=True)
-#         created_at = models.DateTimeField(auto_now_add=True)
-#         image = models.ImageField(upload_to='photos/', verbose_name='Фотография')
-#         group = models.ForeignKey('Group', on_delete=models.CASCADE, related_name='catalogs')
-#         profile = models.OneToOneField('Profile', on_delete=models.CASCADE)
-#         tags = models.ManyToManyField('Tag')
-#
-#         STATUS_CHOICES = [
-#             ('draft', 'Draft'),
-#             ('published', 'Published'),
-#         ]
-#         status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
-#
-#
-#         def __str__(self):
-#             return f"{self.first_name} {self.Last_name}"
-#
-#         class Meta:
-#             verbose_name = 'Каталог'
-#             verbose_name_plural = 'Каталоги'
-#             ordering = ['first_name']
-#             db_table = 'custom_table_name'
