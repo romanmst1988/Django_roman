64aa029f1bed07e0e73384044d55a47f2069529d
Роман Мстиславский <mstislavskijr@gmail.com>
2025-10-14 01:40:50 +0300

    modified catalog/admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,14 +7,11 @@ from .models import Product, Category
 # второй простой способ обширный и гибкий с декоратором
 @admin.register(Product) # декоратор для регистрации модели
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'image', 'category', 'price', 'created_at', 'updated_at') # поля для отображения в админке
-    list_display_links = ('name', 'description') # ссылки на редактирование
-    list_editable = ('price',) # поля для редактирования
-    list_per_page = 10 # количество записей на странице
-    search_fields = ('name', 'description', 'category__name') # поиск по имени, описанию и категории
-    list_filter = ('category', 'created_at', 'updated_at') # фильтр по категории, дате создания и изменения
+    list_display = ('id', 'name','price', 'category') # поля для отображения в админке
+    search_fields = ('name', 'description') # поиск по имени и писанию
+    list_filter = ('category',) # фильтр по категории, дате создания и изменения
 
 @admin.register(Category) # декоратор для регистрации модели
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description') # поля для отображения в админке
+    list_display = ( 'id', 'name') # поля для отображения в админке
     search_fields = ('name', 'description') # поиск по имени и описанию
