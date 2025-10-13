b3f4d3254c060658e9261921eb68084e5ffc65e0
Роман Мстиславский <mstislavskijr@gmail.com>
2025-10-13 17:49:51 +0300

    trening

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Catalog
 
-# Register your models here.
+# admin.site.register(Catalog) # самый простой способ
+
+@admin.register(Catalog)
+class CatalogAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'year')
+    list_filter = ('year',)
+    search_fields = ('first_name', 'last_name',)
--- catalog/models.py
+++ catalog/models.py
@@ -1,34 +1,34 @@
 from django.db import models
 
-# class Catalog(models.Model):
-#     FIRST_YEAR = 'first'
-#     SECOND_YEAR = 'second'
-#     THIRD_YEAR = 'third'
-#     FOURTH_YEAR = 'fourth'
-#
-#     YEAR_IN_SCHOOL_CHOICES = [
-#         (FIRST_YEAR, 'Первый курс'),
-#         (SECOND_YEAR, 'Второй курс'),
-#         (THIRD_YEAR, 'Третий курс'),
-#         (FOURTH_YEAR, 'Четвертый курс'),
-#     ]
-#
-#     first_name = models.CharField(max_length=150, verbose_name='Имя')
-#     last_name = models.CharField(max_length=150, verbose_name='Фамилия')
-#     year = models.CharField(
-#         max_length=6,
-#         choices=YEAR_IN_SCHOOL_CHOICES,
-#         default=FIRST_YEAR,
-#         verbose_name='Курс'
-#     )
-#
-#     def __str__(self):
-#         return f'{self.first_name} {self.last_name}'
-#
-#     class Meta:
-#         verbose_name = 'студент'
-#         verbose_name_plural = 'студенты'
-#         ordering = ['last_name']
+class Catalog(models.Model):
+    FIRST_YEAR = 'Первичная недвижимость'
+    SECOND_YEAR = 'Новостройки'
+    THIRD_YEAR = 'Дома со сроком эксплуатации до X лет'
+    FOURTH_YEAR = 'Старый жилой фонд'
+
+    YEAR_IN_SCHOOL_CHOICES = [
+        (FIRST_YEAR, 'Первичная недвижимость'),
+        (SECOND_YEAR, 'Новостройки'),
+        (THIRD_YEAR, 'Дома со сроком эксплуатации до X лет'),
+        (FOURTH_YEAR, 'Старый жилой фонд'),
+    ]
+
+    first_name = models.CharField(max_length=150, verbose_name='Вид недвижимости')
+    last_name = models.CharField(max_length=150, verbose_name='Площадь (кв.м)', unique=True)
+    year = models.CharField(
+        max_length=66,
+        choices=YEAR_IN_SCHOOL_CHOICES,
+        default=FIRST_YEAR,
+        verbose_name='Состояние объекта'
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'Объект недвижимости'
+        verbose_name_plural = 'Объекты недвижимости'
+        ordering = ['last_name']
 
 # Поля моделей и Параметры полей
 # class Group(models.Model):
