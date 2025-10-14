a1c270ab9471e4beaf18d7b5e96eb900d363f89a
Роман Мстиславский <mstislavskijr@gmail.com>
2025-10-14 15:34:32 +0300

    create fixture

--- catalog/management/commands/add_fixtur.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from django.core.management import call_command
-
-class Command(BaseCommand):
-    help = 'Load test data from fixture'
-
-    def handle(self, *args, **kwargs):
-        call_command('loaddata', 'catalogs_fixture.json')
-        self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
