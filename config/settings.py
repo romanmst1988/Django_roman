0d7bcd8b46641f81804f54ed69cdb396e7af67cb
Роман Мстиславский <mstislavskijr@gmail.com>
2025-11-10 22:36:39 +0300

    set up real messaging for users

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = "users.User"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.yandex.ru"
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from .views import CustomLoginView, ProfileEditView, RegisterView
+from .views import ProductCreateView, ProductUpdateView, RegisterView, CustomLoginView
 
 app_name = "users"
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(next_page="products:list"), name="logout"),
-    path("profile/edit/", ProfileEditView.as_view(), name="profile_edit"),
+    path("logout/", LogoutView.as_view(next_page="products_list"), name="logout"),
+    path("profile/edit/", ProductCreateView.as_view(), name="profile_edit"),
 ]
