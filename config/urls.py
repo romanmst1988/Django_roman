a0c6f8ca4c89ec857bf4a42e2e0a0d6e1f3f9682
Роман Мстиславский <mstislavskijr@gmail.com>
2026-01-27 04:44:50 +0300

    new_Docker_008

--- config/urls.py
+++ config/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # path('', HomePageView.as_view(), name='home'),
     path('', include('catalog.urls')),
-    path("catalog/", include("catalog.urls")),
+    # path("catalog/", include("catalog.urls")),
     path("blog/", include("blog.urls")),
     re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
